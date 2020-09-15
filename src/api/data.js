77de7a8df06567e69cdf8ca255feaac65a49500c
Adrian Wijaya <adrianwijaya100@gmail.com>
2020-09-15 20:31:30 +0700

    [ADD]: data for fetch later

---
+++ src/api/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+  {
+    name: "Portfolio Site (this site)",
+    link: "https://www.adrianwijaya.codes",
+    content:
+      "A Portfolio website fully built using React.js, designed to be responsive and suppor PWA (coming soon)",
+  },
+  {
+    name: "Rubiks 2x2",
+    link: "https://www.rubiks2x2.herokuapp.com/",
+    content:
+      "A Web-based Rubik simulation and solver, built using Prolog in Logic Programming Course",
+  },
+  {
+    name: "Line Chatbot",
+    link: "https://github.com/mon23c/line-chatbot",
+    content:
+      "A Chatbot built using python which has miscellaneous function such as calculator, web scraping, etc",
+  },
+];
+
+export const experiences = [
+  {
+    title:
+      "Web Developer at Faculty of Computer Science, University of Indonesia",
+    time: "Aug 2020 - Present",
+    content: "",
+  },
+  {
+    title: "CS UI Undergraduate Teaching Assistant",
+    time: "2019 - Present",
+    content:
+      "I have been a teaching assistant in several computer science courses (Calculus, Statistic, Data Structures and Algorithms). Most of my work is spent on grading work, giving feedback and sometimes providing assistance ahead of exams",
+  },
+  {
+    title: "Awardee, SEA scholarship",
+    time: "Nov 2018",
+    content: "",
+  },
+  {
+    title: "Finalist, ACM ICPC Regional Jakarta 2019",
+    time: "Oct 2019",
+    content: "",
+  },
+  {
+    title: "Finalist, Junior Programming Contest at Compfest",
+    time: "2017",
+    content: "",
+  },
+  {
+    title: "Second Place, Provincial Olympiad in Informatics (OSP Komputer)",
+    time: "2017",
+    content: "",
+  },
+];
